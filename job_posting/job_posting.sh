7b82b5b622f638414de19b77f6d947ad0babe183
pch229 <115756727+pch229@users.noreply.github.com>
2024-11-29 09:22:49 +0900

    Create job_posting.sh

---
+++ job_posting/job_posting.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# source /home/alpaca/workspace/Crawling/job_posting/job_posting/bin/activate
+source /home/ubuntu/job_crawling/bin/activate
+
+
+# /usr/bin/python3 /home/alpaca/workspace/Crawling/job_posting/Job_posting.py/
+# python /home/alpaca/workspace/Crawling/job_posting/Job_posting.py
+python3 /home/ubuntu/job_posting/main.py
